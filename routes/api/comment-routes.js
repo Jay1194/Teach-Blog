ddcc5cff926b878c665ca15302280e11dc785820
Jay <j.thomastt1@gmail.com>
2024-04-23 03:00:10 -0700

    can now comment on posts and conditional rendering

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -4,14 +4,14 @@ const { Comment, User } = require('../../models');
 router.get('/', (req, res) => {
   console.log('========================');
   Comment.findAll({
-    attributes: ['id', 'comment_text', 'user_id', 'post_id'],
     order: [['created_at', 'DESC']],
+    attributes: ['id', 'comment_text', 'user_id', 'post_id'],
     include: [
       {
         model: User,
-        attributes: ['username'],
-      },
-    ],
+        attributes: ['username']
+      }
+    ]
   })
     .then((dbCommentData) => res.json(dbCommentData))
     .catch((err) => {
@@ -21,23 +21,26 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  Comment.create({
-    comment_text: req.body.comment_text,
-    user_id: req.body.user_id,
-    post_id: req.body.post_id,
-  })
-    .then((dbCommentData) => res.json(dbCommentData))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+  if (req.session) {
+    Comment.create({
+      comment_text: req.body.comment_text,
+      user_id: req.body.user_id,
+      post_id: req.body.post_id,
+      user_id: req.session.user_id
+    })
+      .then((dbCommentData) => res.json(dbCommentData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
 });
 
 router.delete('/:id', (req, res) => {
   Comment.destroy({
     where: {
-      id: req.params.id,
-    },
+      id: req.params.id
+    }
   })
     .then((dbCommentData) => {
       if (!dbCommentData) {
