578299cdc73902512dbfab032733e860227f0122
Jay <j.thomastt1@gmail.com>
2024-04-24 00:41:58 -0700

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const apiRoutes = require('./indexx.js');
+
 const homeRoutes = require('./home-routes');
 const dashboardRoutes = require('./dashboard-routes.js');
 
 router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+
 router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
