7d6b3183374f0e72e15f39e8b48c281ce589c34e
Jay <j.thomastt1@gmail.com>
2024-04-23 23:03:49 -0700

    fix

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: process.env.SESSION_SECRET,
+  secret: 'canyouguessmysecret?',
   cookie: {
     maxAge: threeHours
   },
